5c4a4d6d8959284443522502a2f6062105f0f3f1
Diego Francisco Martinez Reyes <francisco.martinezry@uanl.edu.mx>
2025-08-16 00:25:55 -0600

    Add files via upload

---
+++ Ejercicio5/Ejercicio5.ps1
@@ -0,0 +1,30 @@
+﻿Clear-Host
+function Validar-Archivo{
+    param ([string]$Ruta)
+
+    try{
+        if (Test-Path $Ruta){
+            $contenido = Get-Content $Ruta -ErrorAction Stop
+            return "Archivo encontrado y accesible: $Ruta"
+
+        }else{
+            throw "El archivo no existe"
+
+        }
+    }
+    catch{
+        return "Error: $_"
+    }
+    finally{
+        Write-Host "Validacion finalizada para: $Ruta" -ForegroundColor Cyan
+    }
+    
+}
+
+#Prueba de la funcion
+Validar-Archivo -Ruta "C:\Archivo_inexistente.txt"
+
+#Asegurate de crear un archivo txt con el nombre archivo en el Escritorio
+Validar-Archivo -Ruta "$env:USERPROFILE\Escritorio\archivo.txt"
+
+Validar-Archivo -Ruta "C:\Users\diego\OneDrive\Escritorio\archivo.txt"
